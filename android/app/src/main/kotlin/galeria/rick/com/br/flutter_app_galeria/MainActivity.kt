2eae2caa94fc3974b4d57b9d8d00adcbbed96f1d
RicarteRick <ricarterick@outlook.com>
2020-02-18 21:35:45 -0300

    testando

---
+++ android/app/src/main/kotlin/galeria/rick/com/br/flutter_app_galeria/MainActivity.kt
@@ -0,0 +1,12 @@
+package galeria.rick.com.br.flutter_app_galeria
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
